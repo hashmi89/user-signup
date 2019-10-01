ce9a8095be98fd777c74926dc7a9dccce59c3484
Sikandar Hashmi <sikandar.salman@gmail.com>
2019-10-01 18:44:15 -0400

    Fininshed Assignment

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
 
-from flask import Flask
+from flask import Flask, request, redirect, render_template
+import cgi
+import os
+
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-form = file(templates/index.html)
-
 @app.route("/")
 def index():
-    return form
+    return render_template('index.html')
+
+@app.route("/", methods=["POST"])
+def sign_up():
+    username = request.form["username"]
+    password_input = request.form["password"]
+    verify_password_input = request.form["verify_password"]
+
+    verify_password_error = ""
+
+    if password_input != verify_password_input:
+        verify_password_error = "Passwords do not match!"
+        return render_template('index.html', username=username, verify_password_error=verify_password_error)
+
+    else:
+        return render_template('welcome.html', username=username)
+
 
 app.run()
